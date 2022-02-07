5d09debf5c9a49e222a8737db38e7c6b5f0abb7a
samdhein <hein.sam@gmail.com>
2022-02-07 12:03:47 -0800

    adopted proper variable naming convention

--- FirstJava.java
+++ FirstJava.java
@@ -1,11 +1,11 @@
 public class FirstJava {
     public static void main(String[] args){
-        String FirstLine = "My name is Sam Hein";
-        String SecondLine = "I am 31 years old";
-        String ThirdLine = "My hometown is Manhattan Beach, CA";
+        String firstLine = "My name is Sam Hein";
+        String secondLine = "I am 31 years old";
+        String thirdLine = "My hometown is Manhattan Beach, CA";
 
-        System.out.println(String.format("%s", FirstLine));
-        System.out.println(String.format("%s", SecondLine));
-        System.out.println(String.format("%s", ThirdLine));    
+        System.out.println(String.format("%s", firstLine));
+        System.out.println(String.format("%s", secondLine));
+        System.out.println(String.format("%s", thirdLine));    
     }
 }
